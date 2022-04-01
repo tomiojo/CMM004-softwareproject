cee599bb9afaf2594842232d7d6d611c5ba65dff
GeorGodinez <geor.godinez@gmail.com>
2022-04-01 17:47:59 +0100

    Contact Form

---
+++ message.php
@@ -0,0 +1,24 @@
+<?php
+/*CONTACT FORM CODE*/
+/*(W3Schools, 2022), (CodingNepal, 2021)*/
+  $name = htmlspecialchars($_POST['name']);
+  $email = htmlspecialchars($_POST['email']);
+  $message = htmlspecialchars($_POST['message']);
+  if(!empty($email) && !empty($message)){
+    if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+      $receiver = " "; /* Future site email */
+      $subject = "From: $name <$email>";
+      $body = "Name: $name\nEmail: $email\n\nMessage:\n$message\n\nRegards,\n$name";
+      $sender = "From: $email";
+      if(mail($receiver, $subject, $body, $sender)){
+         echo "Your message has been sent";
+      }else{
+         echo "Sorry, failed to send your message!";
+      }
+    }else{
+      echo "Enter a valid email address!";
+    }
+  }else{
+    echo "Email and message fields are required!";
+  }
+?>
