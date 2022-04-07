ba2191bae997a1088ef4dae2457f852536fc3463
Tomi <79085860+tomiojo@users.noreply.github.com>
2022-04-07 14:55:24 +0100

    Delete demo.sql

--- demo.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE services (
-    ServiceID int(11)NOT NULL AUTO_INCREMENT,
-    servicecategory varchar(255),
-    Companyname varchar(255),
-    Companyaddress varchar(255),
-    Telephone int(255),
-    City varchar(255),
-);
