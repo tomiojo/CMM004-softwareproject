74ba9bceec4ff3a97c8c8eca51e1d0a90edcba7f
sandramtzd <s.martinez-dominguez@rgu.ac.uk>
2022-04-09 12:05:47 +0100

    adding ; to print

--- signupbe.php
+++ signupbe.php
@@ -25,7 +25,7 @@ if(isset($_POST["submit"])){
         exit();
     }
     createUser($con,$email,$password,$telephone);
-    print $sql
+    print $sql;
     }
     else{
     header("location:signup.php");
