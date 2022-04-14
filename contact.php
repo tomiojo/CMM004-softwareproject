6f3496ffd14688d1dd9195628fd3e2afe96b79d4
sandramtzd <s.martinez-dominguez@rgu.ac.uk>
2022-04-14 18:47:07 +0100

    Form page and change in contact

--- contact.php
+++ contact.php
@@ -40,7 +40,7 @@
             <h1>We love to hear from you!</h1>
             <div class="form-center">
             <div class="wrapper">
-               <form action="./message.php">
+               <form action="./form.php">
                  <div class="dbl-field">
                    <div class="field">
                      <input type="text" name="name" placeholder="Enter your name">
